f73959468c9d82d262ca3f64396c24bff59b492c
23030004-oss <23030004@itcelaya.edu.mx>
2025-09-04 16:22:32 -0600

    cambio uno cajero model

--- src/main/java/CajeroAutomatico.java
+++ src/main/java/CajeroAutomatico.java
@@ -9,7 +9,7 @@ import com.mycompany.models.Views.CajeroView;
 public class CajeroAutomatico {
   public static void main(String[]args){
       cajeroModel model=new cajeroModel();
-      CajeroView view=new CajeroView();
+      CajeroView<String> view=new CajeroView<>();
       CajeroControllers controller=new CajeroControllers(model,view);
       controller.iniciarSistema();
   }
